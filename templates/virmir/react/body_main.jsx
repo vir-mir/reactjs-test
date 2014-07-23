6f4b692de098ad58b84e97701283b609bf5c1a04
vir-mir <virmir49@gmail.com>
2014-07-24 02:06:43 +0400

    style transformation a

--- templates/virmir/react/body_main.jsx
+++ templates/virmir/react/body_main.jsx
@@ -4,7 +4,7 @@ var vir_mir_body = React.createClass({
 
     render: function () {
         return (
-            <div className="container-fluid">
+            <div className="container-fluid content__main">
                 <vir_mir_list_post />
                 <vir_mir_list_post />
                 <vir_mir_list_post />
