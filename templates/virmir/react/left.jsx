9119a0e906119a8f86b4b923a360ea22895469ec
vir-mir <virmir49@gmail.com>
2014-07-23 11:30:12 +0400

    add bootstarap

--- templates/virmir/react/left.jsx
+++ templates/virmir/react/left.jsx
@@ -1,36 +1,37 @@
 /** @jsx React.DOM */
 
 var vir_mir_left = React.createClass({
-      render: function() {
+    render: function () {
         return (
-          <div className="pure-menu pure-menu-open left__vir_mir">
-              <a className="pure-menu-heading">Yahoo! Sites</a>
-              <ul>
-                  <li>
-                      <a href="#">Flickr</a>
-                  </li>
-                  <li>
-                      <a href="#">Messenger</a>
-                  </li>
-                  <li>
-                      <a href="#">Sports</a>
-                  </li>
-                  <li>
-                      <a href="#">Finance</a>
-                  </li>
-                  <li class="pure-menu-heading">More Sites!</li>
-                  <li>
-                      <a href="#">Games</a>
-                  </li>
-                  <li>
-                      <a href="#">News</a>
-                  </li>
-                  <li>
-                      <a href="#">OMG!</a>
-                  </li>
-              </ul>
-          </div>
-        );
-      }
-    });
+            <div className="pure-menu pure-menu-open left__vir_mir">
+                <span className="glyphicon glyphicon-search"></span>
+                <a className="pure-menu-heading">Yahoo! Sites</a>
+                <ul>
+                    <li>
+                        <a href="#">Flickr</a>
+                    </li>
+                    <li className="pure-menu-selected">
+                        <a href="#">Messenger</a>
+                    </li>
+                    <li>
+                        <a href="#">Sports</a>
+                    </li>
+                    <li>
+                        <a href="#">Finance</a>
+                    </li>
+                    <li class="pure-menu-heading">More Sites!</li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                    <li>
+                        <a href="#">OMG!</a>
+                    </li>
+                </ul>
+            </div>
+            );
+    }
+});
 
