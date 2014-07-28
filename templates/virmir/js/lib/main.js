f414a2424fe0f460fd80ac74b72911bd6ade5b95
vir-mir <virmir49@gmail.com>
2014-07-29 02:05:57 +0400

    router js

--- templates/virmir/js/lib/main.js
+++ templates/virmir/js/lib/main.js
@@ -2,10 +2,15 @@
  * Created by vir-mir on 23.07.14.
  */
 
+var router = null;
 
 $(function () {
+    router = new Router();
+    router.setRouter();
+
     React.renderComponent(
         vir_mir(null),
         document.getElementById('vir-mir')
     );
 });
+
